f5aa7060bf50d7ed2408eb60cc6723a608a822ea
Evelin <evelin@localhost.localdomain>
2016-06-02 16:51:13 +0200

    Initiated work on creating new blog entries and showing them.

--- app/controllers/blog/entries_controller.rb
+++ app/controllers/blog/entries_controller.rb
@@ -1,7 +1,17 @@
 class Blog::EntriesController < ApplicationController
 	
 	def index
-		@entries = []
+		@entries = Entry.all
+	end
+
+	def new
+		@entry = Entry.new
+	end
+
+	def create
+		@entry = Entry.create(params[:entry].permit(:title, :body))
+		render json: params[:entry]
+		#redirect_to blog_entries_path
 	end
 
 end
